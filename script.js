783e0f3edafeaca1c34dd0205fc8d75cbd57f9aa
elouannh <elouannhosta01@gmail.com>
2024-02-05 15:40:01 +0100

    little counter yeaahhh

---
+++ script.js
@@ -0,0 +1,7 @@
+let value = 0;
+document.addEventListener('DOMContentLoaded', () => alert("Contenu chargé !"));
+
+document.getElementById("counter-button").addEventListener("mousedown", () => {
+    value += 1;
+    document.getElementById("counter-display").innerText = `${value}`;
+});
