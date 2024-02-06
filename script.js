6fc8a7712bd58995cbb3e964d6c632f4d7e60260
elouannh <elouannhosta01@gmail.com>
2024-02-06 11:56:09 +0100

    cleaned sript

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-let value = 0;
-let game = false;
-let timer = 0;
-let scores = [];
-let [posx, posy] = [0, 0];
-const maxTime = 3 * 1000;
+/** @type {[number, number[]]} */
+const [MAX_TIME, scores] = [3 * 1000, []];
+
+/** @type {(HTMLElement | null)[]} */
+const [buttonH, scoresH, counterDisplayH, timerDisplayH] = [
+    document.getElementById("counter-button"),
+    document.getElementById("scores"),
+    document.getElementById("counter-display"),
+    document.getElementById("timer-display"),
+];
+
+let [buttonWidth, buttonHeight] = [buttonH.offsetWidth, buttonH.offsetHeight];
+let [game, value, timerSave, timerTemp, posx, posy] = [false, 0, 0, 0, 0];
 
 document.getElementById("counter-button").addEventListener("mousedown", () => {
     let button = document.getElementById("counter-button");
 
-    if (game === false) {
+    if (!game) {
         game = true;
-        timer = Date.now();
+        timerSave = Date.now();
     }
 
-    let time = Date.now();
-    if (time - timer >= maxTime) {
-        game = false;
+    timerTemp = Date.now();
+    if (timerTemp - timerSave >= MAX_TIME) {
         scores.push(value);
-        timer = 0;
-        value = 0;
+
+        [game, timer, value] = [false, 0, 0];
+
         button.style.top = `500px`;
         button.style.left = `${(window.innerWidth / 2) - 20}px`;
     }
 
-    if (game === false) {
-        document.getElementById("scores").innerText = `${scores.map((e, i) => `${i + 1}. ${e}`).join("\n")}`;
-
-        return;
-    }
-
+    if (!game) return scoresH.innerText = `${scores.map((e, i) => `${i + 1}. ${e}`).join("\n")}`;
+        
     value += 1;
-    document.getElementById("counter-display").innerText = `${value}`;
-    document.getElementById("timer-display").innerText = `${(maxTime - (time - timer)) / 1000}`;
+    counterDisplayH.innerText = `${value}`;
+    timerDisplayH.innerText = `${(MAX_TIME - (timerTemp - timerSave)) / 1000}`;
 
-    let buttonWidth = button.offsetWidth;
-    let buttonHeight = button.offsetHeight;
-
-    [posx, posy] = [Math.floor(Math.random() * (window.innerWidth - buttonWidth)), Math.floor(Math.random() * (window.innerHeight - buttonHeight))];
+    [posx, posy] = [
+        Math.floor(Math.random() * (window.innerWidth - buttonWidth)), Math.floor(Math.random() * (window.innerHeight - buttonHeight))
+    ];
     button.style.top = `${posy}px`;
     button.style.left = `${posx}px`;
 });
